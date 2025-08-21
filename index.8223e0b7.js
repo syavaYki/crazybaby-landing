a6a8628fe2779e742d992b61287947ae8958a49e
syavaYki <syavaYki@users.noreply.github.com>
2025-08-21 16:29:24 +0000

    Deploying build from 14f315760c47eee1ea6b6ada8158af4e1f12c89c 14f315760c47eee1ea6b6ada8158af4e1f12c89c

---
+++ index.8223e0b7.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{spaceBetween:30,slidesPerView:1,effect:"slide",pagination:{el:".swiper-pagination",type:"fraction"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.8223e0b7.js.map
